f9aa196161c306cc60fa3b8f368685d62a7060f5
Sardar Ali Hassan <135510461+Sardarhassan12@users.noreply.github.com>
2026-02-10 11:08:37 +0500

    feat: export booking components from index

---
+++ src/components/booking/index.js
@@ -0,0 +1,11 @@
+/**
+ * Booking Components Index
+ * Export all booking-related components
+ */
+
+export { default as BookingCard } from "./BookingCard";
+export { default as BookingFilters } from "./BookingFilters";
+export { default as BookingStats } from "./BookingStats";
+export { default as BookingStatusBadge } from "./BookingStatusBadge";
+export { default as PaymentStatusBadge } from "./PaymentStatusBadge";
+export { default as TourTypeBadge } from "./TourTypeBadge";
